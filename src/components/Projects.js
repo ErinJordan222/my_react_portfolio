161e29decb5f38a678b33b19ef520e19a551377e
ErinJordan222 <erin.jordan@du.edu>
2022-06-16 22:53:35 -0600

    added_project_titles

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -23,6 +23,7 @@ function Projects() {
             <div className="col-sm-12">
                 <div className="card mb-3">
                 <img src={weather} alt="weather_screenshot"></img>
+                <h4 className="project-title">Weather Dashboard App</h4>
           <div className="project-icons">
               <a href="https://github.com/ErinJordan222/Weather_Dashboard.git"><img src={github} alt="github_icon"/></a> 
               <a href="https://erinjordan222.github.io/Weather_Dashboard/"><img src={global} alt="global_icon"/></a> 
@@ -34,6 +35,7 @@ function Projects() {
             <div className="col-sm-12">
                 <div className="card mb-3">
                 <img src={codequiz} alt="codequiz_screenshot"></img>
+                <h4 className="project-title">Javascript Code Quiz</h4>
           <div className="project-icons">
               <a href="https://github.com/ErinJordan222/code_quiz.git"><img src={github} alt="github_icon"/></a> 
               <a href="https://erinjordan222.github.io/code_quiz/"><img src={global} alt="global_icon"/></a>
@@ -44,6 +46,7 @@ function Projects() {
             <div className="col-sm-12">
                 <div className="card mb-3">
                 <img src={readmegenerator} alt="readme generator_screenshot"></img>
+                <h4 className="project-title">README Generator</h4>
           <div className="project-icons">
               <a href="https://github.com/ErinJordan222/README_Generator.git"><img src={github} alt="github_icon"/></a> 
               
@@ -54,6 +57,7 @@ function Projects() {
             <div className="col-sm-12">
                 <div className="card mb-3">
                 <img src={notetaker} alt="notetaker_screenshot"></img>
+                <h4 className="project-title">Note Taker Express.js App</h4>
           <div className="project-icons">
               <a href="https://github.com/ErinJordan222/Note_Pad_11.git"><img src={github} alt="github_icon"/></a> 
               <a href="https://note-pad-du-11.herokuapp.com/"><img src={global} alt="global_icon"/></a>
@@ -64,9 +68,8 @@ function Projects() {
             <div className="col-sm-12">
                 <div className="card mb-3">
                 <img src={schedular} alt="schedular_screenshot"></img>
-                
+                <h4 className="project-title">Scheduling Calendar App</h4>
           <div className="project-icons">
-
               <a href="https://github.com/ErinJordan222/ScheduleApp.git"><img src={github} alt="github_icon"/></a> 
               <a href="https://erinjordan222.github.io/ScheduleApp/"><img src={global} alt="global_icon"/></a>
               </div>
@@ -76,6 +79,7 @@ function Projects() {
             <div className="col-sm-12">
                 <div className="card mb-3">
                 <img src={teamgenerator} alt="teamgenerator_screenshot"></img>
+                <h4 className="project-title">Team Profile Generator</h4>
           <div className="project-icons">
               <a href="https://github.com/ErinJordan222/Team_Profile_Generator.git"><img src={github} alt="github_icon"/></a> 
               
@@ -86,6 +90,7 @@ function Projects() {
             <div className="col-sm-12">
                 <div className="card mb-3">
                 <img src={techblog} alt="techblog_screenshot"></img>
+                <h4 className="project-title">Tech Blog App</h4>
           <div className="project-icons">
               <a href="https://github.com/ErinJordan222/tech-blog.git"><img src={github} alt="github_icon"/></a> 
               <a href="https://tech-blog-du-14-2022.herokuapp.com/"><img src={global} alt="global_icon"/></a>
@@ -96,6 +101,7 @@ function Projects() {
             <div className="col-sm-12">
                 <div className="card mb-3">
                 <img src={groupproject2} alt="groupproject2_screenshot"></img>
+                <h4 className="project-title">Group Project- My-Interests Podcast Search App</h4>
           <div className="project-icons">
               <a href="https://github.com/JakeAwsd/my-intrests-project.git"><img src={github} alt="github_icon"/></a> 
               <a href="https://project-2-du-coding-bootcamp.herokuapp.com/"><img src={global} alt="global_icon"/></a>
@@ -106,6 +112,7 @@ function Projects() {
             <div className="col-sm-12">
                 <div className="card mb-3">
                 <img src={socialnetworkapi} alt="socialnetworkapi_screenshot"></img>
+                <h4 className="project-title">Social Network API</h4>
           <div className="project-icons">
               <a href="https://github.com/ErinJordan222/Social-Network_API.git"><img src={github} alt="github_icon"/></a> 
               
@@ -116,6 +123,7 @@ function Projects() {
             <div className="col-sm-12">
                 <div className="card mb-3">
                 <img src={groupproject1} alt="groupproject1_screenshot"></img>
+                <h4 className="project-title">Group Project- Coder Blog App</h4>
           <div className="project-icons">
               <a href="https://github.com/hugo180/thecoding-blog.git"><img src={github} alt="github_icon"/></a> 
               
