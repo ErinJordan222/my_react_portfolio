a9e0a79bd147e5ec2894a84a5618cd5998ee6142
ErinJordan222 <erin.jordan@du.edu>
2022-10-19 10:31:36 -0600

    contact update

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import github from './Contact/github.png';
-import linkedin from './Contact/linkedin.png';
 import email from './Contact/email.png';
+import linkedin from './Contact/linkedin.png';
 import cellphone from './Contact/cellphone.png';
+import github from './Contact/github.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Contact() {
@@ -13,14 +13,14 @@ function Contact() {
  <h1 className="contact">Contact Me</h1>
  <div className="contact-container">
  <div className="wrapper">
-  
+ <a href="mailto:erinjordan2790@gmail.com"><img src={email} alt="email_icon"/></a>
+ <a href="https://www.linkedin.com/in/erin-jordan-6b58a51a0/"><img src={linkedin} alt="linkedin_icon"/></a>
+ <a href="tel:8104447591"><img src={cellphone} alt="phone_icon"/></a>
  <a href="https://github.com/ErinJordan222"><img src={github} alt="github_icon"/></a>
      
-     <a href="https://www.linkedin.com/in/erin-jordan-6b58a51a0/"><img src={linkedin} alt="linkedin_icon"/></a>
      
-     <a href="mailto:erinjordan2790@gmail.com"><img src={email} alt="email_icon"/></a>
+     
     
-     <a href="tel:8104447591"><img src={cellphone} alt="phone_icon"/></a>
 
      
 </div>
